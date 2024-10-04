03cd156fbd1cc661da81b811670eff49b3d0fa70
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2024-10-04 12:33:56 +0200

    Remove PWA bits

--- javascript.js
+++ javascript.js
@@ -303,24 +303,3 @@ document.addEventListener('click', function (event) {
         }
     }
 });
-
-const registerServiceWorker = async () => {
-    if ("serviceWorker" in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register("./service-worker.js", {
-                scope: "/",
-            });
-            if (registration.installing) {
-                console.log("Service worker installing");
-            } else if (registration.waiting) {
-                console.log("Service worker installed");
-            } else if (registration.active) {
-                console.log("Service worker active");
-            }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
-        }
-    }
-};
-
-registerServiceWorker();
